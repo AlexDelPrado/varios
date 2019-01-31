7f3edbb712c7b3075e649dc282d2b55011937f8f
Alejandro Del Prado <31344555+AlexDelPrado@users.noreply.github.com>
2019-01-31 19:27:03 +0100

    Add files via upload

--- pong.c
+++ pong.c
@@ -1,50 +1,65 @@
 #include <stdio.h>
-#define Y 20 //vertical
-#define X 80 //horizontal
+#define V 20 //Vertical
+#define H 60 //Horizontal
 
-void inicio(char campo[Y][X], int pelX, int pelY, int iniJugador, int finJugador, int iniPc, int finPc);
-void borde(char campo[Y][X]);
-void raqJug(char campo[Y][X], int iniJugador, int finJugador);
-void raqPc(char campo[Y][X], int iniPc, int finPc);
-void pelota(char campo[Y][X], int pelX, int pelY);
-void leerCampo(char campo[Y][X]);
+void inicio(char campo[V][H], int pelY, int pelX, int inicioJugador1, int finalJugador1, int inicioJugador2, int finalJugador2);
+void cuadrado(char campo[V][H]);
+void pelota(char [V][H], int pelY, int pelX);
+void jugador1(char campo[V][H], int inicioJugador1, int finalJugador1);
+void jugador2(char campo[V][H], int inicioJugador2, int finalJugador2);
+void leerCampo(char campo[V][H]);
+void gameLoop(char campo[V][H], int pelY, int pelX, int inicioJugador1, int finalJugador1, int inicioJugador2, int finalJugador2, int modPelX, int modPelY, int modJugador2);
+void draw(char [V][H]);
 
-int main() {
-	
-	int pelX, pelY, iniJugador, finJugador, iniPc, finPc;
-	char campo[Y][X];
 
-	pelX = 40;
-	pelY = 10;
 
-	iniJugador = 8;
-	finJugador = 12;
+int main(){
 
-	iniPc = 8;
-	finPc = 12;
+    //Variables posicion
+    int pelX, pelY;
+    int inicioJugador1, finalJugador1;
+    int inicioJugador2, finalJugador2;
+    int modPelX, modPelY, modJugador2; // Varibales modificacion
+    
+    char campo[V][H];
+    
+    // Inicializacion varibales posicion
+    pelX = H / 2;
+    pelY = V / 2;
 
-	inicio(campo, pelX,pelY,iniJugador,finJugador, iniPc,finPc);
-	leerCampo(campo);
-	system("pause");
-	return 0;
+    inicioJugador1 = 7;
+    finalJugador1 = 12;
+
+    inicioJugador2 = 7;
+    finalJugador2 = 12;
+
+    //Inicializacion varibles modificacion
+    modPelX = -1;
+    modPelY = -1;
+    modJugador2 = -1;
+
+
+    inicio(campo,pelY, pelX, inicioJugador1, finalJugador1, inicioJugador2, finalJugador2);
+    gameLoop(campo,pelY, pelX, inicioJugador1, finalJugador1, inicioJugador2, finalJugador2,modPelX,modPelY,modJugador2);
+    
+    return 0;
+}
+void inicio(char campo[V][H], int pelY, int pelX, int inicioJugador1, int finalJugador1, int inicioJugador2, int finalJugador2){
+    cuadrado(campo);
+    pelota(campo, pelX, pelY);
+    jugador1(campo, inicioJugador1, finalJugador1);
+    jugador2(campo, inicioJugador2, finalJugador2);
 }
 
-void inicio(char campo[Y][X], int pelX, int pelY, int iniJugador, int finJugador, int iniPc, int finPc) {
-	borde(campo);
-	raqJug(campo, iniJugador, finJugador);
-	raqPc(campo, iniPc, finPc);
-	pelota(campo, pelX, pelY);
-}
+void cuadrado(char campo[V][H]){
+    int j, i;
 
-void borde(char campo[Y][X]) {
-	int j, i;
-
-	for (i = 0; i < Y; i++) {
-		for (j = 0; j < X; j++) {
-			if (i == 0 || i == Y - 1) {
+	for (i = 0; i < V; i++) {
+		for (j = 0; j < H; j++) {
+			if (i == 0 || i == V - 1) {
 				campo[i][j] = '-';
 			}
-			else if (j == 0 || j == X-1) {
+			else if (j == 0 || j == H-1) {
 				campo[i][j] = '|';
 			}
 			else {
@@ -53,35 +68,63 @@ void borde(char campo[Y][X]) {
 		}
 	}
 }
-void raqJug(char campo[Y][X], int iniJugador, int finJugador) {
-	int i, j;
-
-	for (i = iniJugador; i <= finJugador; i++) {
-		for (j = 2; j <= 3; j++) {
-			campo[i][j] = 'x';
-		}
-	}
+void pelota(char campo[V][H], int pelY, int pelX){
+    campo[pelX][pelY] = 'O';
 }
-void raqPc(char campo[Y][X],int iniPc,int finPc) {
-	int i, j;
 
-	for (i = iniPc; i <= finPc; i++) {
-		for (j = X - 4; j <= X - 3; j++) {
-			campo[i][j] = 'x';
-		}
-	}
+void jugador1(char campo[V][H], int inicioJugador1, int finalJugador1){
+    int i,j;
+    for(i = inicioJugador1; i <= finalJugador1; i++ ){
+        for(j = 2; j <= 3; j++){
+            campo[i][j] = 'S';
+        }
+    }
+}
+void jugador2(char campo[V][H], int inicioJugador2, int finalJugador2){
+    int i,j;
+    for(i = inicioJugador2; i <= finalJugador2; i++){
+        for(j = H - 4  ; j <= H - 3 ; j++){
+            campo[i][j] = 'W';
+        }
+    }
+}
+
+void leerCampo(char campo[V][H]){
+    int i,j;
+
+    for (i = 0; i < V; i++){
+        for(j=0;j<H;j++){
+            printf("%c", campo[i][j]);
+        }
+        printf("\n");
+    }
+}
+void gameLoop(char campo[V][H], int pelY, int pelX, int inicioJugador1, int finalJugador1, int inicioJugador2, int finalJugador2, int modPelX, int modPelY, int modJugador2){
+    int gol;
+    gol = 0;
+    
+    do{
+        draw(campo); // Dibujar en pantalla
+        input(campo, &pelX, &pelY, &inicioJugador1, &finJugador1, &inicioJugador2, &finJugador2, &modPelX, &modPelY, &modJugador2, &gol); // Verificar y modificar posiciones
+        upDate(); // Actualizar la matriz campo
+        Sleep(10);
+
+    } while (gol == 1);
+}
+
+void draw(char [V][H]){
+    system ("cls");
+    leerCampo(campo);
 
 }
-void pelota(char campo[Y][X], int pelX, int pelY) {
-	campo[pelY][pelX] = '0';
-}
-void leerCampo(char campo[Y][X]) {
-	int i, j;
+void input(char campo[V][H], int *pelX, int *pelY, int *inicioJugador1, int *finJugador1, int  *inicioJugador2, int *finJugador2, int *modPelX, int *modPelY, int *modJugador2, int *gol){
+    //Verificacion
+    if (pelY == 1 || pelY == V -2){
+        *modPelY *= -1;
+    }
 
-	for (i = 0; i < Y; i++) {
-		for (j = 0; j < X; j++) {
-			printf("%c", campo[i][j]);
-		}
-		printf("\n");
-	}
+    //Modificacion
+}
+void upDate(){
+
 }
